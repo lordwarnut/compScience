a00bf497eb638f63eb5693bcbc9a9bdf5e79337c
Shannon Blackhall <SWBlackhall@gmail.com>
2018-09-29 20:05:17 +1200

    add BusFrame class initialisation.

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ Simple Bus Booking System with tkinter gui
 import tkinter as tk
 
 
-class booking:
+class Booking:
     def __init__(self):
         pass
 
@@ -42,10 +42,29 @@ class WelcomeFrame(tk.Frame):
 
 
 class BusFrame(tk.Frame):
+    bus_fstring = "Bus Service to {}"
+    date_fstring = "Date : {}"
+    seat_fstring = "Seats Available  : {}"
+    bunk_fstring = "Bunks Available : {}"
+
     def __init__(self, parent):
         super().__init__(parent)
 
         self.parent = parent
+
+        self.label = tk.Label(self, text=self.bus_fstring.format(""))
+        self.label.grid(row=0, column=0, sticky='w')
+
+        self.grid_rowconfigure(1, minsize=10)
+
+        self.datelabel = tk.Label(self, text=self.date_fstring.format(""))
+        self.datelabel.grid(row=2, column=0, sticky='w')
+
+        self.seatlabel = tk.Label(self, text=self.seat_fstring.format(""))
+        self.seatlabel.grid(row=3, column=0, sticky='w')
+
+        self.bunklabel = tk.Label(self, text=self.bunk_fstring.format(""))
+        self.bunklabel.grid(row=4, column=0, sticky='w')
 
 
 class MainApp(tk.Frame):
@@ -55,6 +74,11 @@ class MainApp(tk.Frame):
         self.bus = Bus()
         self.parent = parent
 
+        self.busframe = BusFrame(self)
+        self.busframe.grid(row=0, column=0)
+
+        self.dataframe = WelcomeFrame(self)
+        self.dataframe.grid(row=0, column=1)
 
 def main():
     root = tk.Tk()
